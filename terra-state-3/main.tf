f10cce2dc90b5b026f74b7ce3857f4c351863fcc
kevin <newkster95@gmail.com>
2020-12-04 14:12:03 -0800

    terraform state management s3

--- terra-state-3/main.tf
+++ terra-state-3/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "terraform_state" {
-  bucket = "kevin-terraform-state"
+  bucket = "kevin-newk-terraform-state"
 
   #prevent accidental deletion of this s3 bucket
   lifecycle {
@@ -23,4 +23,38 @@ resource "aws_s3_bucket" "terraform_state" {
       }
     }
   }
+}
+
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "terraform-up-and-running-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
+
+terraform {
+  backend "s3" {
+    #replacing this with bucket name
+    bucket = "kevin-newk-terraform-state"
+    key    = "global/s3/terraform.tfstate"
+    region = "us-east-2"
+
+    #replacing this with DynamoDB table name
+    dynamodb_table = "terraform-up-and-running-locks"
+    encrypt        = true
+  }
+}
+
+output "s3_bucket_arn" {
+  value       = aws_s3_bucket.terraform_state.arn
+  description = "The ARN of the S3 bucket"
+}
+
+output "dynamodb_table_name" {
+  value       = aws_dynamodb_table.terraform_locks.name
+  description = "The name of the DynamoDB table"
 }
